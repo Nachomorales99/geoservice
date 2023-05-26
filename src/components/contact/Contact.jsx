ef6cb63d6fc43fc5959763771841e1da92163449
Nachomorales99 <nacho.morales5@gmail.com>
2023-05-26 19:00:27 -0300

    Contact view

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,26 +1,27 @@
 import Link from 'next/link';
+import Map from '../googleMaps/Map';
 
 export default function Contact() {
 	return (
 		<>
-			<section>
-				<header className="flex flex-col items-center justify-center">
-					<h1>Contactenos</h1>
+			<section className="flex flex-col items-center justify-center">
+				<header className="flex flex-col items-center justify-center m-8">
+					<h1>Contáctenos</h1>
 					<p>
 						Expertos en suelos, geotécnica y perforaciones. Tu aliado integral
 						para proyectos sólidos y exitosos
 					</p>
 				</header>
-				<div className="flex flex-row">
-					<div className="flex flex-col">
-						<div className="flex flex-row">
+				<div className="flex flex-row mt-8">
+					<div className="flex flex-col mr-8">
+						<div className="flex flex-row mb-8">
 							<div className="flex flex-col items-center justify-center">
 								<img
 									src="https://res.cloudinary.com/nacho-morales/image/upload/v1684951076/Geoservice/Iconos/Map-icon_cwq8yv.png"
 									alt="Map"
 								/>
 
-								<p>UBICACIÓN:</p>
+								<h3>UBICACIÓN:</h3>
 								<p>Adolfo Calle 350, Godoy Cruz, Mendoza</p>
 							</div>
 							<div className="flex flex-col items-center justify-center">
@@ -28,19 +29,19 @@ export default function Contact() {
 									src="https://res.cloudinary.com/nacho-morales/image/upload/v1684948476/Geoservice/Iconos/Mail-icon_puvbsi.png"
 									alt="Mail"
 								/>
-								<p>EMAIL:</p>
+								<h3>EMAIL:</h3>
 								<Link href="mailto:geoservice@geoservice.com.ar">
 									<p>geoservice@geoservice.com.ar</p>
 								</Link>
 							</div>
 						</div>
-						<div className="flex flex-row">
+						<div className="flex flex-row mt-8">
 							<div className="flex flex-col items-center justify-center">
 								<img
 									src="https://res.cloudinary.com/nacho-morales/image/upload/v1684948476/Geoservice/Iconos/Phone-icon_lpceym.png"
 									alt="Phone"
 								/>
-								<p>LLÁMANOS:</p>
+								<h3>LLÁMANOS:</h3>
 								<p>+5492614241129</p>
 							</div>
 							<div className="flex flex-col items-center justify-center">
@@ -48,13 +49,13 @@ export default function Contact() {
 									src="https://res.cloudinary.com/nacho-morales/image/upload/v1684948476/Geoservice/Iconos/Reloj-icon_ttysdh.png"
 									alt="Clock"
 								/>
-								<p>HORARIO</p>
+								<h3>HORARIO</h3>
 								<p>Lunes a Viernes</p>
 								<p>9:00 a 13:00 - 16:00 a 19:00 horas</p>
 							</div>
 						</div>
 					</div>
-					<div>MAPA</div>
+					<Map />
 				</div>
 			</section>
 		</>
---
+++ src/components/googleMaps/Map.jsx
@@ -0,0 +1,23 @@
+'use client';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
+export default function Map() {
+	const containerStyle = {
+		width: '600px',
+		height: '400px',
+		marginLeft: '2rem',
+	};
+
+	const center = {
+		lat: -32.9167748,
+		lng: -68.8413216,
+	};
+
+	return (
+		<LoadScript googleMapsApiKey="AIzaSyB0neCs61kY9ygCM7nTAITVTLOz2vvYnkY">
+			<GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
+				<Marker position={{ lat: -32.9167748, lng: -68.8413216 }} />
+			</GoogleMap>
+		</LoadScript>
+	);
+}
