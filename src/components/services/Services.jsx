9b38d1e1791170202764a73a723c25064616219b
Nachomorales99 <nacho.morales5@gmail.com>
2023-06-15 00:30:14 -0300

    Machines

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -3,18 +3,19 @@ import { services } from '../../constants/services.json';
 export default function Services() {
 	return (
 		<>
-			{services.map((service, index) => (
+			{services.map((el, index) => (
 				<div key={index}>
-					<h1 style={{ color: 'red' }}>{service.name}</h1>
+					<h1 style={{ color: 'red' }}>{el.name}</h1>
+					<img src={el.img} alt={el.name} />
 					<h2 style={{ color: 'blue' }}>Servicios:</h2>
 					<ul>
-						{service.service.map((item, itemIndex) => (
+						{el.service.map((item, itemIndex) => (
 							<li key={itemIndex}>{item}</li>
 						))}
 					</ul>
 					<h2 style={{ color: 'orange' }}>Clientes:</h2>
 					<ul>
-						{service.clients.map((item, itemIndex) => (
+						{el.clients.map((item, itemIndex) => (
 							<li key={itemIndex}>{item}</li>
 						))}
 					</ul>
