867a7fae6294a8bdca2272a3ad7213fbfe5c98ba
Juan Ignacio Morales <nacho.morales5@gmail.com>
2023-06-22 01:22:48 -0300

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,13 +9,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
         <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="https://res.cloudinary.com/nacho-morales/image/upload/v1684860890/Geoservice/Logo_hd_pjie2t.png"
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='https://res.cloudinary.com/nacho-morales/image/upload/v1684860890/Geoservice/Logo_hd_pjie2t.png'
         />
       </head>
       <body>
