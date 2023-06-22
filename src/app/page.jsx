a9690b893b2e122711a2d51c8a0dbbc8f9c77836
Juan Ignacio Morales <nacho.morales5@gmail.com>
2023-06-22 01:30:52 -0300

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,24 +5,21 @@ import Machines from '@/components/machines/Machines.jsx'
 import { roboto } from '@/utils/font.js'
 
 export default function Home () {
-	return (
-		<>
-			<Header />
-			<div className='flex items-center justify-center'>
-				<div className='shadow-xl p-4 mx-5 text-center flex items-center justify-center w-fit'>
-					<h1
-						className={`mx-2 m-2 text-xl text-marron_oscuro font-bold w-fit ${roboto.className}`}
-					>
-						"Expertos en suelos, geotécnica y perforaciones. Tu aliado integral
-						para proyectos sólidos y exitosos"
-					</h1>
-				</div>
-			</div>
-			<div className='flex justify-center flex-col items-center rounded-sm'>
-				<Services />
-			</div>
-			<Machines />
-			<Sponsor />
-		</>
+return (
+<>
+<Header />
+<div className='flex items-center justify-center'>
+<div className='shadow-xl p-4 mx-5 text-center flex items-center justify-center w-fit'>
+<h1 className={`mx-2 m-2 text-xl text-marron_oscuro font-bold w-fit ${roboto.className}`} >
+"Expertos en suelos, geotécnica y perforaciones. Tu aliado integralpara proyectos sólidos y exitosos"
+</h1>
+</div>
+</div>
+<div className='flex justify-center flex-col items-center rounded-sm'>
+<Services />
+</div>
+<Machines />
+<Sponsor />
+</>
 	)
 }
