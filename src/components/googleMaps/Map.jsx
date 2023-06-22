9c999e8c1cb2f1c1334ebd139ea086bca94472ab
Nachomorales99 <nacho.morales5@gmail.com>
2023-06-22 16:46:58 -0300

    api key

--- src/components/googleMaps/Map.jsx
+++ src/components/googleMaps/Map.jsx
@@ -15,7 +15,7 @@ export default function Map() {
 	};
 
 	return (
-		<LoadScript googleMapsApiKey={process.env.API_KEY_GOOGLE_MAPS}>
+		<LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_G_KEY}>
 			<GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
 				<Marker position={{ lat: -32.9167748, lng: -68.8413216 }} />
 			</GoogleMap>
