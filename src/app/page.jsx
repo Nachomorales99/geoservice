6a43ac8e21b9d2dd63c0eff2f3cd4fbb34e24cd6
Juan Ignacio Morales <nacho.morales5@gmail.com>
2023-06-22 01:27:23 -0300

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,15 @@
-import Sponsor from '../components/sponsor/Sponsor.jsx';
-import Header from '@/components/header/Header.jsx';
-import Services from '@/components/services/Services.jsx';
-import Machines from '@/components/machines/Machines.jsx';
-import { roboto } from '@/utils/font.js';
+import Sponsor from '../components/sponsor/Sponsor.jsx'
+import Header from '@/components/header/Header.jsx'
+import Services from '@/components/services/Services.jsx'
+import Machines from '@/components/machines/Machines.jsx'
+import { roboto } from '@/utils/font.js'
 
-export default function Home() {
+export default function Home () {
 	return (
 		<>
 			<Header />
-			<div className="flex items-center justify-center">
-				<div className=" shadow-xl p-4 mx-5 text-center flex items-center justify-center w-fit">
+			<div className='flex items-center justify-center'>
+				<div className='shadow-xl p-4 mx-5 text-center flex items-center justify-center w-fit'>
 					<h1
 						className={`mx-2 m-2 text-xl text-marron_oscuro font-bold w-fit ${roboto.className}`}
 					>
@@ -18,11 +18,11 @@ export default function Home() {
 					</h1>
 				</div>
 			</div>
-			<div className="flex justify-center flex-col items-center rounded-sm">
+			<div className='flex justify-center flex-col items-center rounded-sm'>
 				<Services />
 			</div>
 			<Machines />
 			<Sponsor />
 		</>
-	);
+	)
 }
