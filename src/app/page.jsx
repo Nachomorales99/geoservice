f6ef08a6508c7906ef88962c42f303403ed4ae77
Juan Ignacio Morales <nacho.morales5@gmail.com>
2023-06-22 01:44:19 -0300

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,16 +8,16 @@ export default function Home () {
   return (
     <>
       <Header />
-  <div className='flex items-center justify-center'>
+      <div className='flex items-center justify-center'>
     <div className='shadow-xl p-4 mx-5 text-center flex items-center justify-center w-fit'>
-    <h1 className={`mx-2 m-2 text-xl text-marron_oscuro font-bold w-fit ${roboto.className}`} >
+      <h1 className={`mx-2 m-2 text-xl text-marron_oscuro font-bold w-fit ${roboto.className}`} >
 "Expertos en suelos, geotécnica y perforaciones. Tu aliado integralpara proyectos sólidos y exitosos"
-    </h1>
+      </h1>
     </div>
   </div>
       <div className='flex justify-center flex-col items-center rounded-sm'>
-    <Services />
-  </div>
+        <Services />
+      </div>
       <Machines />
       <Sponsor />
     </>
